4d3bc444f39f35a0db3c0c02ebd81ea8118bbe26
Sébastien <sebastien.civade700@gmail.com>
2023-10-30 13:27:29 +0100

    Commit 30/10

--- app/src/main/java/fr/scivade/hyrulecompendium/MainActivity.kt
+++ app/src/main/java/fr/scivade/hyrulecompendium/MainActivity.kt
@@ -2,10 +2,13 @@ package fr.scivade.hyrulecompendium
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Switch
+import androidx.appcompat.widget.SwitchCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
 }
